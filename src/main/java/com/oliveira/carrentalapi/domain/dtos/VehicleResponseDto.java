9598ca18655ac1bbce7a49da7a2248b2e5ec68e4
MacPro <macpro@MacBook-Pro-de-MacPro.local>
2024-05-03 14:06:24 -0300

    Refactor ResponseEntity Vehicle

---
+++ src/main/java/com/oliveira/carrentalapi/domain/dtos/VehicleResponseDto.java
@@ -0,0 +1,14 @@
+package com.oliveira.carrentalapi.domain.dtos;
+
+import java.util.UUID;
+
+public record VehicleResponseDto(
+        UUID id,
+        String model,
+        String plate,
+        String color,
+        Boolean complete,
+        Integer mileage,
+        Boolean ative,
+        String categoryName) {
+}
