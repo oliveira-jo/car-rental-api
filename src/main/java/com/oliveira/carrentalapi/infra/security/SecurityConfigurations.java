52c45d0c3730c541c8600af9fa5abc4d470bf5c7
devjoliveira <macpro@MacBook-Pro-de-MacPro.local>
2025-03-18 20:52:54 -0300

    Add a actuator in project to see the app metrics

--- src/main/java/com/oliveira/carrentalapi/infra/security/SecurityConfigurations.java
+++ src/main/java/com/oliveira/carrentalapi/infra/security/SecurityConfigurations.java
@@ -21,7 +21,7 @@ public class SecurityConfigurations {
 
   private static final String[] AUTH_WHITELIST = {
       "/api/v1/auth/**", "/v3/api-docs/**", "/v3/api-docs/yaml",
-      "/swagger-ui/**", "/swagger-ui.html"
+      "/swagger-ui/**", "/swagger-ui.html", "/actuator/**"
   };
 
   public SecurityConfigurations(SecurityFilter securityFilter) {
